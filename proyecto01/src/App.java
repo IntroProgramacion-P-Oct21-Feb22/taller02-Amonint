8b1c364ba3a4874dce754e3f3602829438b62330
Amon <you@example.com>
2021-10-22 11:16:25 -0500

    primer commit

---
+++ proyecto01/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ejemplo básico de Java");
+        System.out.println("Versionado a través de Git");
+        System.out.println("Manejado desde GitHub");
+    }
+}
