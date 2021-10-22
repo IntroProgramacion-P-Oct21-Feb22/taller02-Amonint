fad6da81ccfcbd395e23e94aacc8837c0419cc35
Amon <you@example.com>
2021-10-22 11:32:29 -0500

    Subi mal el primer commit commit 2

--- proyecto01/src/App.java
+++ proyecto01/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Ejemplo dos básico de Java");
+        System.out.println("Versionado a través de Git");
+        System.out.println("Manejado desde GitHub");
+        System.out.println("Además se usar GitHubClassroom");
     }
 }
